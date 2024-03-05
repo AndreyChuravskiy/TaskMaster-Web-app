0608a2804fdf293a80352885ca858a07f43f21f3
AndreyChuravskiy <375292816898@yandex.com>
2024-03-06 01:24:55 +0300

    hello

---
+++ src/main/java/org/example/taskmasterwebapp/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.example.taskmasterwebapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/hello").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/org/example/taskmasterwebapp/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.taskmasterwebapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/api/hello")
+public class HelloController {
+
+    @GetMapping String hello() {
+        return "hello";
+    }
+
+}
