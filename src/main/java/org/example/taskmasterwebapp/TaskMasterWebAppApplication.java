2c2fd21be23520dd162935bb18f44050e5ac31dd
AndreyChuravskiy <375292816898@yandex.com>
2024-03-06 01:01:46 +0300

    start

---
+++ src/main/java/org/example/taskmasterwebapp/TaskMasterWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.taskmasterwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMasterWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMasterWebAppApplication.class, args);
+	}
+
+}
